129c44090d9d413a31459c0a78f895b514abcf8e
Bsian <bsian@staff.libraryofcode.us>
2019-10-19 13:31:06 +0100

    Finished help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,5 @@
 import { Message } from 'eris';
+import { createPaginationEmbed } from 'eris-pagination';
 import { Client, Util, config } from '..';
 import { Command, RichEmbed } from '../class';
 
@@ -8,6 +9,7 @@ export default class Help extends Command {
     this.name = 'help';
     this.description = 'Display a list of commands';
     this.usage = `${config.prefix}help | ${config.prefix}help ping`;
+    this.aliases = ['commands'];
     this.enabled = true;
   }
 
@@ -29,10 +31,35 @@ export default class Help extends Command {
         return { name: `${config.prefix}${c.name}`, value: `**Description:** ${c.description}\n**Aliases:** ${aliases}\n**Usage:** ${c.usage}\n${displayedPerms}`, inline: true };
       });
 
-      const embed = new RichEmbed();
-      embed.setTimestamp(); embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL);
-      embed.setAuthor(`${this.client.user.username}#${this.client.user.discriminator}`, this.client.user.avatarURL);
-      embed.setDescription(`Command list for ${this.client.user.username}`);
+      const splitCommands = this.util.splitFields(commands);
+      const cmdPages: RichEmbed[] = [];
+      splitCommands.forEach((splitCmd) => {
+        const embed = new RichEmbed();
+        embed.setTimestamp(); embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL);
+        embed.setAuthor(`${this.client.user.username}#${this.client.user.discriminator}`, this.client.user.avatarURL);
+        embed.setDescription(`Command list for ${this.client.user.username}`);
+        splitCmd.forEach((c) => embed.addField(c.name, c.value, c.inline));
+        return cmdPages.push(embed);
+      });
+      // @ts-ignore
+      if (cmdPages.length === 1) return message.channel.createMessage({ embed: cmdPages[0] });
+      return createPaginationEmbed(message, this.client, cmdPages);
     }
+    const foundCommand = this.util.resolveCommand(cmd);
+    if (!foundCommand) return message.channel.createMessage(`${this.client.stores.emojis.error} **Command not found!**`);
+    const perms: string[] = [];
+    let allowedRoles = foundCommand.permissions && foundCommand.permissions.roles && foundCommand.permissions.roles.map((r) => `<@&${r}>`).join(', ');
+    if (allowedRoles) { allowedRoles = `**Roles:** ${allowedRoles}`; perms.push(allowedRoles); }
+    let allowedUsers = foundCommand.permissions && foundCommand.permissions.users && foundCommand.permissions.users.map((u) => `<@${u}>`).join(', ');
+    if (allowedUsers) { allowedUsers = `**Users:** ${allowedUsers}`; perms.push(allowedUsers); }
+    const displayedPerms = perms.length ? `**Permissions:**\n${perms.join('\n')}` : '';
+    const aliases = foundCommand.aliases.map((alias) => `${config.prefix}${alias}`).join(', ');
+    const embed = new RichEmbed();
+    embed.setTimestamp(); embed.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL);
+    embed.setTitle(`${config.prefix}${foundCommand.name}`); embed.setAuthor(`${this.client.user.username}#${this.client.user.discriminator}`, this.client.user.avatarURL);
+    const description = `**Description**: ${foundCommand.description}\n**Usage:** ${foundCommand.usage}\n**Aliases:** ${aliases}\n**Permissions**: ${displayedPerms}`;
+    embed.setDescription(description);
+    // @ts-ignore
+    return message.channel.createMessage({ embed });
   }
 }
