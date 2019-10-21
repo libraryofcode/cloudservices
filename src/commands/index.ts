bd343d19e04a33b5d88dd9df750b6e7659f1f00a
Bsian <bsian@staff.libraryofcode.us>
2019-10-21 01:25:39 +0100

    Added command exports cuz matt's lazy af

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,6 @@
 export { default as Lock } from './lock';
 export { default as Modlogs } from './modlogs';
 export { default as Ping } from './ping';
+export { default as Announce } from './announce';
+export { default as CWG } from './cwg';
+export { default as Help } from './help';
