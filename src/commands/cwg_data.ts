a5cf30652c725ec009e817d67374f5b41c1ec989
Bsian <bsian@staff.libraryofcode.us>
2019-11-01 23:53:21 +0000

    Support multiple domains using a single port

--- src/commands/cwg_data.ts
+++ src/commands/cwg_data.ts
@@ -1,6 +1,7 @@
 import fs from 'fs-extra';
 import moment from 'moment';
 import x509 from '@ghaiklor/x509';
+import { createPaginationEmbed } from 'eris-pagination';
 import { Message } from 'eris';
 import { Command, RichEmbed } from '../class';
 import { Client } from '..';
@@ -18,21 +19,25 @@ export default class CWG_Data extends Command {
   public async run(message: Message, args: string[]) {
     try {
       if (!args[0]) return this.client.commands.get('help').run(message, ['cwg', this.name]);
-      const domain = await this.client.db.Domain.findOne({ $or: [{ domain: args[0] }, { port: Number(args[0]) || '' }] });
-      if (!domain) return message.channel.createMessage(`***${this.client.stores.emojis.error} The domain or port you provided could not be found.***`);
-      const embed = new RichEmbed();
-      embed.setTitle('Domain Information');
-      embed.addField('Account Username', domain.account.username, true);
-      embed.addField('Account ID', domain.account.userID, true);
-      embed.addField('Domain', domain.domain, true);
-      embed.addField('Port', String(domain.port), true);
-      embed.addField('Certificate Issuer', x509.getIssuer(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).organizationName, true);
-      embed.addField('Certificate Subject', x509.getSubject(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).commonName, true);
-      embed.addField('Certificate Expiration Date', moment(x509.parseCert(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).notAfter).format('dddd, MMMM Do YYYY, h:mm:ss A'), true);
-      embed.setFooter(this.client.user.username, this.client.user.avatarURL);
-      embed.setTimestamp();
+      const dom = await this.client.db.Domain.find({ $or: [{ domain: args[0] }, { port: Number(args[0]) || '' }] });
+      if (!dom.length) return message.channel.createMessage(`***${this.client.stores.emojis.error} The domain or port you provided could not be found.***`);
+      const embeds = dom.map(async (domain) => {
+        const embed = new RichEmbed();
+        embed.setTitle('Domain Information');
+        embed.addField('Account Username', domain.account.username, true);
+        embed.addField('Account ID', domain.account.userID, true);
+        embed.addField('Domain', domain.domain, true);
+        embed.addField('Port', String(domain.port), true);
+        embed.addField('Certificate Issuer', x509.getIssuer(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).organizationName, true);
+        embed.addField('Certificate Subject', x509.getSubject(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).commonName, true);
+        embed.addField('Certificate Expiration Date', moment(x509.parseCert(await fs.readFile(domain.x509.cert, { encoding: 'utf8' })).notAfter).format('dddd, MMMM Do YYYY, h:mm:ss A'), true);
+        embed.setFooter(this.client.user.username, this.client.user.avatarURL);
+        embed.setTimestamp();
+        return embed;
+      });
       // @ts-ignore
-      return message.channel.createMessage({ embed });
+      if (embeds.length === 1) return message.channel.createMessage({ embed: embeds[0] });
+      return createPaginationEmbed(message, this.client, embeds, {});
     } catch (error) {
       return this.client.util.handleError(error, message, this);
     }
