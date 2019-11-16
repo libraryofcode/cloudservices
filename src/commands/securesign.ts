802d18541fbd1706cde5e919f8f5c75ba732272d
Bsian <bsian@staff.libraryofcode.us>
2019-11-16 22:27:51 +0000

    Added commands (not complete)

---
+++ src/commands/securesign.ts
@@ -0,0 +1,25 @@
+import { Message } from 'eris';
+import { Command } from '../class';
+import { Client } from '..';
+import Build from './securesign_build';
+import Init from './securesign_init';
+
+export default class SecureSign extends Command {
+  constructor(client: Client) {
+    super(client);
+    this.name = 'securesign';
+    this.description = 'Runs SecureSign CLI commands';
+    this.usage = `Run ${this.client.config.prefix}${this.name} [subcommand] for usage information`;
+    this.aliases = ['ss'];
+    this.subcmds = [Build, Init];
+    this.enabled = true;
+  }
+
+  public async run(message: Message) {
+    try {
+      return this.client.commands.get('help').run(message, [this.name]);
+    } catch (error) {
+      return this.client.util.handleError(error, message, this);
+    }
+  }
+}
---
+++ src/commands/securesign_build.ts
@@ -0,0 +1,36 @@
+import { Message } from 'eris';
+import { Client } from '..';
+import { Command, RichEmbed } from '../class';
+
+export default class SecureSign_Build extends Command {
+  constructor(client: Client) {
+    super(client);
+    this.name = 'build';
+    this.description = 'Shows information about the current build of the CLI';
+    this.usage = `${this.client.config.prefix}securesign build`;
+    this.enabled = true;
+  }
+
+  public async run(message: Message, args: string[]) {
+    try {
+      const msg = await message.channel.createMessage(`${this.client.stores.emojis.loading} ***Loading build information...***`);
+
+      const build = await this.client.util.exec("sudo -H -u root bash -c 'securesign build'");
+      const info = build.replace(/\n/g, '\n**').replace(/:/g, ':**').split('\n');
+      const title = info.shift();
+      const description = info.join('\n');
+      const content = null;
+
+      const embed = new RichEmbed();
+      embed.setTitle(title);
+      embed.setDescription(description);
+      embed.setAuthor(this.client.user.username, this.client.user.avatarURL);
+      embed.setFooter(`Requested by ${message.member.username}#${message.member.discriminator}`, message.member.avatarURL);
+
+      // @ts-ignore
+      msg.edit({ content, embed });
+    } catch (error) {
+      this.client.util.handleError(error, message, this);
+    }
+  }
+}
