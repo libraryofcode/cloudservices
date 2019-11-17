35519fd9486edbac00c1f58669d4d0342c7b1d45
Matthew R <matthew@staff.libraryofcode.org>
2019-11-16 20:13:35 -0500

    Fix issue with auth

--- src/api/Security.ts
+++ src/api/Security.ts
@@ -43,15 +43,11 @@ export default class Security {
   }
 
   public extractBearer(req: Request): string {
-    const url = new URL(req.url);
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
       return req.headers.authorization.split(' ')[1];
     }
     if (req.query && req.query.token) {
       return req.query.token;
-    }
-    if (url.password) {
-      return url.password;
     }
     return 'null';
   }
