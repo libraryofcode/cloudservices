1ec893b0fa80d912380486a7153e2f68cb2f619f
Matthew R <matthew@staff.libraryofcode.org>
2019-11-16 19:26:39 -0500

    update class index

--- src/class/index.ts
+++ src/class/index.ts
@@ -2,3 +2,4 @@ export { default as Command } from './Command';
 export { default as RichEmbed } from './RichEmbed';
 export { default as Util } from './Util';
 export { default as Collection } from './Collection';
+export { default as Route } from './Route';
