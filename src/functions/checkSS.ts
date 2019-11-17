31b9c6972856514b7023e7d46414404fe0fed633
Bsian <bsian@staff.libraryofcode.us>
2019-11-17 01:46:55 +0000

    SecureSign hash error handler

--- src/functions/checkSS.ts
+++ src/functions/checkSS.ts
@@ -4,22 +4,26 @@ import { Client } from '..';
 
 export default function checkSS(client: Client) {
   setInterval(async () => {
-    const accounts = await client.db.Account.find();
-    const hashes = accounts.filter((h) => h.hash);
-    for (const { hash, userID } of hashes) {
-      try {
-        await axios({
-          method: 'get',
-          url: 'https://api.securesign.org/account/details',
-          headers: { Authorization: hash },
-        });
-      } catch (error) {
-        const { status } = error.response;
-        if (status === 400 || status === 401 || status === 403 || status === 404) {
-          client.db.Account.updateOne({ hash }, { $set: { hash: null } });
-          client.getDMChannel(userID).then((channel) => channel.createMessage('Your SecureSign password has been reset - please reinitialize your SecureSign account')).catch();
+    try {
+      const accounts = await client.db.Account.find();
+      const hashes = accounts.filter((h) => h.hash);
+      for (const { hash, userID } of hashes) {
+        try {
+          await axios({
+            method: 'get',
+            url: 'https://api.securesign.org/account/details',
+            headers: { Authorization: hash },
+          });
+        } catch (error) {
+          const { status } = error.response;
+          if (status === 400 || status === 401 || status === 403 || status === 404) {
+            client.db.Account.updateOne({ hash }, { $set: { hash: null } });
+            client.getDMChannel(userID).then((channel) => channel.createMessage('Your SecureSign password has been reset - please reinitialize your SecureSign account')).catch();
+          }
         }
       }
+    } catch (error) {
+      client.util.handleError(error);
     }
   }, 60000);
 }
