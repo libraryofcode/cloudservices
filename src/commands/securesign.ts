af136b1b6e0db686b0e0155bd0839c6cd26b42fb
Bsian <bsian@staff.libraryofcode.us>
2019-11-19 15:45:22 +0000

    Added createcrt command

--- src/commands/securesign.ts
+++ src/commands/securesign.ts
@@ -5,6 +5,7 @@ import Build from './securesign_build';
 import Init from './securesign_init';
 import Account from './securesign_account';
 import ActivateKey from './securesign_activatekey';
+import CreateCrt from './securesign_createcrt';
 
 export default class SecureSign extends Command {
   constructor(client: Client) {
@@ -13,7 +14,7 @@ export default class SecureSign extends Command {
     this.description = 'Runs SecureSign CLI commands';
     this.usage = `Run ${this.client.config.prefix}${this.name} [subcommand] for usage information`;
     this.aliases = ['ss'];
-    this.subcmds = [Build, Init, Account, ActivateKey];
+    this.subcmds = [Build, Init, Account, ActivateKey, CreateCrt];
     this.enabled = true;
   }
 
