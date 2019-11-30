7bc601b68570201a2653b88420955154983880b0
Bsian <bsian@staff.libraryofcode.us>
2019-11-30 23:53:23 +0000

    Perms fix

--- src/commands/load.ts
+++ src/commands/load.ts
@@ -8,6 +8,7 @@ export default class Ping extends Command {
     this.name = 'load';
     this.description = '(Re)loads command, config or util';
     this.aliases = ['reload'];
+    this.permissions = { users: ['253600545972027394', '278620217221971968'] };
     this.enabled = true;
   }
 
