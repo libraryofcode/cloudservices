cb097067b5b1123a07c357abc89c5824ee09f321
Matthew R <matthew@staff.libraryofcode.org>
2019-12-13 19:23:48 -0500

    fix for disk blocking

--- src/intervals/storage.ts
+++ src/intervals/storage.ts
@@ -6,16 +6,18 @@ export default async function storage(client: Client) {
   const main = async () => {
     const accounts = await client.db.Account.find();
     for (const account of accounts) {
-      const res = await client.util.exec(`du -bs /home/${account.username}`);
-      let bytes = Number(res.split('/')[0].replace('\t', ''));
-      try {
-        await fs.access(`/var/mail/${account.username}`, fs.constants.F_OK);
-        const res2 = await client.util.exec(`du -bs /var/mail/${account.username}`);
-        bytes += Number(res2.split('/')[0].replace('\t', ''));
-      } catch {
-        bytes += 0;
-      }
-      await client.redis.set(`storage-${account.username}`, bytes);
+      setTimeout(async () => {
+        const res = await client.util.exec(`du -bs /home/${account.username}`);
+        let bytes = Number(res.split('/')[0].replace('\t', ''));
+        try {
+          await fs.access(`/var/mail/${account.username}`, fs.constants.F_OK);
+          const res2 = await client.util.exec(`du -bs /var/mail/${account.username}`);
+          bytes += Number(res2.split('/')[0].replace('\t', ''));
+        } catch {
+          bytes += 0;
+        }
+        await client.redis.set(`storage-${account.username}`, bytes);
+      }, 600000);
     }
   };
   await main();
