2fa5a2a5ff71b27afffdaab6935dcc275530c238
Bsian <bsian@staff.libraryofcode.us>
2020-01-01 15:46:19 +0000

    try this

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 # This file builds the Go binaries. Hardcoded by LOC Engineering
-CGO_ENABLED=0 go build -o dist/bin/storage src/go/storage/storage.go src/go/storage/dirsize.go
+go build -o dist/bin/storage src/go/storage/storage.go src/go/storage/dirsize.go
 file dist/bin/storage
-CGO_ENABLED=0 go build -o dist/bin/checkCertificate src/go/checkCertificate/checkCertificate.go
+go build -o dist/bin/checkCertificate src/go/checkCertificate/checkCertificate.go
 file dist/bin/checkCertificate
-CGO_ENABLED=0 go build -o dist/bin/checkCertSignatures src/go/checkCertSignatures/checkCertSignatures.go
-file /dist/bin/checkCertSignatures
+
